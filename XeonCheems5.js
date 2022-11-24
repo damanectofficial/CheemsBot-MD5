6c2e2e3f2677b88bc7800e09166bfdaba343ea35
damanectofficial <88991901+damanectofficial@users.noreply.github.com>
2022-11-24 21:43:30 +0700

    Update XeonCheems5.js

--- XeonCheems5.js
+++ XeonCheems5.js
@@ -2060,31 +2060,6 @@ View list of Messages With ${prefix}listmsg`)
             case 'ping': case 'botstatus': case 'statusbot': {
                 XeonBotInc.sendMessage(m.chat, caption: `👀 *${m.pushName}*\n\n Pong🏓` }, { quoted: m })
             }
-            break
-            case 'p': {
-            	m.reply('Ucap salam yang bener!')
-            teks = `*${pushname} Salam yang benar lah masbro *\n\n Harus diruqiah keknya ni bocah`
-let buttons = [
-{buttonId: `owner`, buttonText: {displayText: 'Owner 🙋🏻‍♂️ '}, type: 1}
-]
-let buttonMessage = {
-image: {url: `https://i.ibb.co/RBDnt3C/images-2022-11-24-T212108-477.jpg`},
-jpegThumbnail: log0,
-caption: teks,
-footer: `${botname}`,
-buttons: buttons,
-headerType: 4,
-contextInfo:{externalAdReply:{
-title:"Ucap Salam Yang Bener!",
-body: "Mikir Cok!", 
-thumbnail: fs.readFileSync("XeonMedia/theme/cheemspic.jpg"),
-mediaType:1,
-mediaUrl: 'https://i.ibb.co/RBDnt3C/images-2022-11-24-T212108-477.jpg',
-sourceUrl: "https://i.ibb.co/RBDnt3C/images-2022-11-24-T212108-477.jpg"
-}}
-}
-XeonBotInc.sendMessage(m.chat, buttonMessage, { quoted: m })
-}
             
             break
             	
